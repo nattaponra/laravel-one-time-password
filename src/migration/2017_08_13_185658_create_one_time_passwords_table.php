8b381ae99bb331a33913fbdca65fbf7f01ded838
Nattapon Rakthong <nattapon@3dsinteractive.com>
2017-08-14 19:51:50 +0700

    Add migration file.

---
+++ src/migration/2017_08_13_185658_create_one_time_passwords_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOneTimePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('one_time_passwords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('otp_code')->index();
+            $table->string('status')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('one_time_passwords');
+    }
+}
