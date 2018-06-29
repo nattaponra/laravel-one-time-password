462187e896dd212c21c765d6bdb73c25644bf917
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-29 14:51:29 +0700

    Add config

--- src/config/config.php
+++ src/config/config.php
@@ -1,10 +1,28 @@
 <?php
 return
     [
-        'otp_default_sms_service' => env('OTP_DEFAULT_SMS_SERVICE', null),
+        'otp_default_service' => env('OTP_DEFAULT_SMS_SERVICE', null),
 
-        'opt_reference_number_length'=>8,
+        'services' => [
+
+            'ClickSendSMS'=> [
+                'username' => env("CLICKSEND_USERNAME",null),
+                'api_key'  => env("CLICKSEND_API_KEY",null),
+                'sms_from' => env("CLICKSEND_SMS_FROM",null),
+
+            ],
+
+            'TwilioSMS'=>[
+                'auth_token'       => env("TWILIO_AUTH_TOKEN",null),
+                'auth_account_sid' => env("TWILIO_AUTH_ACCOUNT_SID",null),
+                'my_phone_number'  => env("TWILIO_MY_PHONE_NUMBER",null),
+
+            ]
+        ],
+
+        'opt_reference_number_length'=> 8,
 
         'opt_digit_length'=> 4
 
     ];
+
