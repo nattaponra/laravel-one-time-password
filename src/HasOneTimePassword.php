655037a8cf43f46550d7558371c86c1c71e2322f
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-29 14:13:17 +0700

    Create  HasOneTimePassword trait

---
+++ src/HasOneTimePassword.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace nattaponra\LaravelOneTimePassword;
+
+
+trait HasOneTimePassword
+{
+
+
+    private function initOTP($opt)
+    {
+        $opt->create([
+            'user_id' => $this->id,
+            'status'  => 'clean-sheet'
+        ]);
+    }
+
+    public function OTP()
+    {
+        $opt = $this->hasOne(OneTimePassword::class, "user_id", "id");
+
+        if ($opt->count() == 0) {
+            $this->initOTP($opt);
+            $opt = $this->hasOne(OneTimePassword::class , "user_id", "id");
+        }
+        return $opt;
+    }
+}
