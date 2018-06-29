3a9f5f013a67290a117b37328164df1d7e2d2ca8
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-29 14:36:44 +0700

    Create opt config file

---
+++ src/config/config.php
@@ -0,0 +1,10 @@
+<?php
+return
+    [
+        'otp_default_sms_service' => env('OTP_DEFAULT_SMS_SERVICE', null),
+
+        'opt_reference_number_length'=>8,
+
+        'opt_digit_length'=> 4
+
+    ];
