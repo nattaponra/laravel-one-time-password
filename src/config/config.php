b9a819a80e5bf2c2d722d3e0b38552afc036f928
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-29 18:16:41 +0700

    Add encode option

--- src/config/config.php
+++ src/config/config.php
@@ -1,18 +1,18 @@
 <?php
 return
     [
-        'otp_default_service' => env('OTP_DEFAULT_SMS_SERVICE', null),
+        'otp_default_service' => env('OTP_DEFAULT_SMS_SERVICE', 'clickend'),
 
         'services' => [
 
-            'ClickSendSMS'=> [
+            'clickend'=> [
                 'username' => env("CLICKSEND_USERNAME",null),
                 'api_key'  => env("CLICKSEND_API_KEY",null),
                 'sms_from' => env("CLICKSEND_SMS_FROM",null),
 
             ],
 
-            'TwilioSMS'=>[
+            'twilio'=>[
                 'auth_token'       => env("TWILIO_AUTH_TOKEN",null),
                 'auth_account_sid' => env("TWILIO_AUTH_ACCOUNT_SID",null),
                 'my_phone_number'  => env("TWILIO_MY_PHONE_NUMBER",null),
@@ -22,7 +22,8 @@ return
 
         'opt_reference_number_length'=> 8,
 
-        'opt_digit_length'=> 4
+        'opt_digit_length'=> 4,
 
+        'encode_password' => false
     ];
 
